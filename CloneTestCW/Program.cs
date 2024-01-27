82c20f7092b98645dc352250667893f5bea617c0
Ryan Pannell <rap23641@email.vccs.edu>
2024-01-27 17:21:21 -0500

    first commit

---
+++ CloneTestCW/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CloneTestCW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
