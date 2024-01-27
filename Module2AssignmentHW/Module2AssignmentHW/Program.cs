3cba2dd8fc80e05ee4b716c19e5d9ef847c31da7
Ryan Pannell <rap23641@email.vccs.edu>
2024-01-27 18:17:00 -0500

    module 2 assignment complete

---
+++ Module2AssignmentHW/Module2AssignmentHW/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Module2AssignmentHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who is being seen today?");
+
+            string patientName = Console.ReadLine();
+
+            Console.WriteLine("Welcome: " + patientName);
+        }
+    }
+}
