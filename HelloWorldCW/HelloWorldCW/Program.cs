e2249753b8a075b1fd0d204e0cf0f93b54fd3b07
Ryan Pannell <rap23641@email.vccs.edu>
2024-01-27 18:10:07 -0500

    classwork module 2

---
+++ HelloWorldCW/HelloWorldCW/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace HelloWorldCW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + userName);
+
+            Console.ReadKey();
+        }
+    }
+}
