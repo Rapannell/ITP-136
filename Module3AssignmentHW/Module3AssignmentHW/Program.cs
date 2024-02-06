67d2cdd40cbbd47617f62c9d6f816deea2a754a6
Ryan Pannell <rap23641@email.vccs.edu>
2024-02-05 22:28:40 -0500

    module 3 assignment complete

---
+++ Module3AssignmentHW/Module3AssignmentHW/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Module3AssignmentHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double oilChange, tires, inspection;
+            double total;
+            string inputString;
+            double tax;
+
+            Console.WriteLine("Thank you for visiting Reynolds Repair Shop!");
+
+            Console.WriteLine("Enter the total for oil change:");
+            inputString = Console.ReadLine();
+            oilChange = Convert.ToDouble(inputString);
+
+            Console.WriteLine("Enter the total for tires:");
+            inputString = Console.ReadLine();
+            tires = Convert.ToDouble(inputString);
+
+            Console.WriteLine("Enter the total for inspection:");
+            inputString = Console.ReadLine();
+            inspection = Convert.ToDouble(inputString);
+
+            total = oilChange + tires + inspection;
+            Console.WriteLine("Your subtotal is $" + total.ToString("F2"));
+
+            tax = total * 0.06;
+            Console.WriteLine("Tax: $" + tax.ToString("F2"));
+
+            total = total + tax;
+            Console.WriteLine("Your grand total is $" + total.ToString("F2"));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
