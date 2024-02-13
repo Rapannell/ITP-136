0b53d5467186d543d541154fe6dd80f09315ca9d
Ryan Pannell <rap23641@email.vccs.edu>
2024-02-13 01:04:46 -0500

    module 4 assignment complete

---
+++ Module4AssignmentHW/Module4AssignmentHW/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Module4AssignmentHW
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome! Please select your reason for this visit");
+            Console.WriteLine("a. Doctor - Sick Appointment");
+            Console.WriteLine("b. Doctor - Check-up");
+            Console.WriteLine("Please type either a or b:");
+            char choice = Console.ReadLine().ToLower()[0];
+
+            switch (choice)
+            {
+                case 'a':
+                    SickAppointment();
+                    break;
+                case 'b':
+                    Checkup();
+                    break;
+            }
+        }
+        static void SickAppointment()
+        {
+            Console.WriteLine("Is the patient a child or an adult? (Enter 'child' or 'adult'):");
+            string patientType = Console.ReadLine().ToLower();
+            int baseVisitCost;
+            if (patientType == "child")
+            {
+                baseVisitCost = 50;
+            }
+            else
+            {
+                baseVisitCost = 75;
+            }
+
+            Console.WriteLine("Were labs done during the appointment? (yes/no):");
+            string labsDone = Console.ReadLine().ToLower();
+            int labsCost;
+            if (labsDone == "yes")
+            {
+                labsCost = 25;
+            }
+            else
+            {
+                labsCost = 0;
+            }
+
+            int totalCost = baseVisitCost + labsCost;
+            Console.WriteLine($"Total cost for the sick appointment: ${totalCost}.00");
+        }
+
+        static void Checkup()
+        {
+            Console.Write("Is the patient a child or an adult? (Enter 'child' or 'adult'): ");
+            string patientType = Console.ReadLine().ToLower();
+            int checkupCost;
+            if (patientType == "child")
+            {
+                checkupCost = 75;
+            }
+            else
+            {
+                checkupCost = 100;
+            }
+
+            Console.WriteLine($"Total cost for the check-up: ${checkupCost}.00");
+        }
+    }
+}
