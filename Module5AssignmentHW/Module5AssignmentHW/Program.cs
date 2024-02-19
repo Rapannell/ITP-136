8e9bacb15c389713d2c924ff4cc3388694c11466
Ryan Pannell <rap23641@email.vccs.edu>
2024-02-19 10:24:59 -0500

    module 5 assignment complete

---
+++ Module5AssignmentHW/Module5AssignmentHW/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Module5AssignmentHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber, endNumber, counter;
+
+            Console.WriteLine("Enter the start number:");
+            startNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the end number:");
+            endNumber = Convert.ToInt32(Console.ReadLine());
+
+            counter = startNumber;
+
+            while (counter <= endNumber)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
