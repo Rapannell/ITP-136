375932b6840e36a530e38e419bd4a1acd0a10c66
Ryan Pannell <rap23641@email.vccs.edu>
2024-04-14 01:42:45 -0400

    module 11 assignment complete

---
+++ Module12AssignmentHW/Part.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using static System.Console;
+
+namespace Module12AssignmentHW
+{
+    public class Part
+    {
+        int partNum;
+        string partName, partDescription;
+        decimal cost;
+
+        public Part(int pNum, string pName, string pD, decimal c)
+        {
+            partNum = pNum;
+            PartName = pName;
+            partDescription = pD;
+            cost = c;
+        }
+
+        public int PartNumber
+        { 
+            get {  return partNum; } 
+            set {  partNum = value; }
+        }
+
+        public string PartName
+        { 
+            get { return partName; }
+            set { partName = value; }
+        }
+
+        public string PartDescription
+        { 
+            get { return partDescription; }
+            set { partDescription = value; }
+        }
+
+        public decimal Cost
+        { 
+            get { return cost; }
+            set { cost = value; }
+        }
+    }
+}
---
+++ Module12AssignmentHW/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using static System.Console;
+
+namespace Module12AssignmentHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numParts;
+
+            Write("How many parts would you like to enter?: ");
+
+            numParts = Convert.ToInt32(ReadLine());
+
+            Part[] partArray = new Part[numParts];
+
+            for (int x = 0; x < partArray.Length; x++)
+            {
+                populatePart(ref partArray[x]);
+            }
+
+            WriteLine("Menu of parts:");
+            for (int i = 0; i < partArray.Length; i++)
+            {
+                WriteLine($"{i + 1} - {partArray[i].PartName}");
+            }
+
+            Write("Please select the number of the part you would like to view: ");
+            int choice = Convert.ToInt32(ReadLine()) - 1;
+
+            if (choice >= 0 && choice < partArray.Length)
+            {
+                WriteLine($"Part Number: {partArray[choice].PartNumber}");
+                WriteLine($"Part Name: {partArray[choice].PartName}");
+                WriteLine($"Part Description: {partArray[choice].PartDescription}");
+                WriteLine($"Part Cost: ${partArray[choice].Cost}");
+            }
+
+            static void populatePart(ref Part e)
+            {
+                Write("Enter part number: ");
+                int partNum = Convert.ToInt32(ReadLine());
+
+                Write("Enter part name: ");
+                string partName = ReadLine();
+
+                Write("Enter part description: ");
+                string partDescription = ReadLine();
+
+                Write("Enter part cost: ");
+                decimal cost = Convert.ToDecimal(ReadLine());
+
+                e = new Part(partNum, partName, partDescription, cost);
+            }
+
+
+
+        }
+    }
+}
